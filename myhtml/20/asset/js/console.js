39856efbcea3df07e3495ce3495fab413077be2e
TarikManoar <tarikmanoar@gmail.com>
2019-09-24 21:06:34 +0600

    fix some bug and make more responsive

---
+++ myhtml/20/asset/js/console.js
@@ -0,0 +1,22 @@
+ console.log(`                                                                                                                                                      
+   SSSSSSSSSSSSSSS WWWWWWWW                           WWWWWWWWIIIIIIIIIIFFFFFFFFFFFFFFFFFFFFFFTTTTTTTTTTTTTTTTTTTTTTT     IIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTT
+ SS:::::::::::::::SW::::::W                           W::::::WI::::::::IF::::::::::::::::::::FT:::::::::::::::::::::T     I::::::::IT:::::::::::::::::::::T
+S:::::SSSSSS::::::SW::::::W                           W::::::WI::::::::IF::::::::::::::::::::FT:::::::::::::::::::::T     I::::::::IT:::::::::::::::::::::T
+S:::::S     SSSSSSSW::::::W                           W::::::WII::::::IIFF::::::FFFFFFFFF::::FT:::::TT:::::::TT:::::T     II::::::IIT:::::TT:::::::TT:::::T
+S:::::S             W:::::W           WWWWW           W:::::W   I::::I    F:::::F       FFFFFFTTTTTT  T:::::T  TTTTTT       I::::I  TTTTTT  T:::::T  TTTTTT
+S:::::S              W:::::W         W:::::W         W:::::W    I::::I    F:::::F                     T:::::T               I::::I          T:::::T        
+ S::::SSSS            W:::::W       W:::::::W       W:::::W     I::::I    F::::::FFFFFFFFFF           T:::::T               I::::I          T:::::T        
+  SS::::::SSSSS        W:::::W     W:::::::::W     W:::::W      I::::I    F:::::::::::::::F           T:::::T               I::::I          T:::::T        
+    SSS::::::::SS       W:::::W   W:::::W:::::W   W:::::W       I::::I    F:::::::::::::::F           T:::::T               I::::I          T:::::T        
+       SSSSSS::::S       W:::::W W:::::W W:::::W W:::::W        I::::I    F::::::FFFFFFFFFF           T:::::T               I::::I          T:::::T        
+            S:::::S       W:::::W:::::W   W:::::W:::::W         I::::I    F:::::F                     T:::::T               I::::I          T:::::T        
+            S:::::S        W:::::::::W     W:::::::::W          I::::I    F:::::F                     T:::::T               I::::I          T:::::T        
+SSSSSSS     S:::::S         W:::::::W       W:::::::W         II::::::IIFF:::::::FF                 TT:::::::TT           II::::::II      TT:::::::TT      
+S::::::SSSSSS:::::S          W:::::W         W:::::W          I::::::::IF::::::::FF                 T:::::::::T           I::::::::I      T:::::::::T      
+S:::::::::::::::SS            W:::W           W:::W           I::::::::IF::::::::FF                 T:::::::::T           I::::::::I      T:::::::::T      
+ SSSSSSSSSSSSSSS               WWW             WWW            IIIIIIIIIIFFFFFFFFFFF                 TTTTTTTTTTT           IIIIIIIIII      TTTTTTTTTTT      
+`);
+console.log(
+    '%cযথেষ্ট প্র্যাক্টিস ছাড়া কোড শিখা অসম্ভব!',
+    'font-size: 18px; background-color: #1ad; color: #323330; font-weight: bolder; padding: 3px 5px; border-radius: 4px'
+  )
