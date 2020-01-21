569bc0de82601ad968d034d81b61e2a01f72fc8d
Manoar <tarikmanoar@gmail.com>
2020-01-21 22:48:54 +0600

    {Create login Form}

--- umraserv-front/asset/js/myJs.js
+++ umraserv-front/asset/js/myJs.js
@@ -36,7 +36,7 @@ $.scrollUp({
 //=====================================================================================
 //   Smooth Scroll
 //=====================================================================================
-$('a').smoothScroll({
+$('a#scrollUp').smoothScroll({
 duration: 1000, // animation speed
 easing: 'swing', // find more easing options on http://api.jqueryui.com/easings/
 offset: 0 // custom offset
