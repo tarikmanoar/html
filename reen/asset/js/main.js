e9b976d9a2ef6d461af3f7299faaa85f7f3ff10f
Manoar <tarikmanoar@gmail.com>
2020-02-24 11:04:16 +0600

    ==[T]== Make REEN responsive

--- reen/asset/js/main.js
+++ reen/asset/js/main.js
@@ -32,7 +32,7 @@ $(function () {
                 items: 1
             },
             600: {
-                items: 1
+                items: 2
             },
             1000: {
                 items: 4
