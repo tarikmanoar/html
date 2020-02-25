dc4a40cc4cb7d0d0391341ee086f02571188a57d
Manoar <tarikmanoar@gmail.com>
2020-02-25 19:55:49 +0600

    ==[T]==

---
+++ immobiliere/asset/js/main.js
@@ -0,0 +1,31 @@
+$(function () { 
+    /*================
+    = SCROLL TO TOP
+    ================*/
+    $.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 1200,
+        animation: 'fade'
+    });
+    /*================
+    = WOW JS INIT
+    ================*/
+    new WOW().init();
+    //Sticky Top
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+// Sticky Div
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+        $('#sticky-anchor').height($('#sticky').outerHeight());
+    } else {
+        $('#sticky').removeClass('stick');
+        $('#sticky-anchor').height(0);
+    }
+}
